Correct rotation and translation

Mads_leg/3D_NS/3D_Pipe.py
@@ -75,7 +75,6 @@ def run(cfg: ModulusConfig) -> None:
     Pipe_domain.add_constraint(Inlet, "Inlet")
     
     # Outlet
-    direction = (outlet_pipe_length * cos(theta + pi / 2), outlet_pipe_length * sin(theta + pi / 2))
     
     Outlet = PointwiseBoundaryConstraint(
         nodes = nodes,
@@ -126,16 +125,23 @@ def run(cfg: ModulusConfig) -> None:
     # )
     # Pipe_domain.add_constraint(integral, "Integral")
     
-    data_path = f"/zhome/e1/d/168534/Desktop/Bachelor_PINN/PINN_Bachelor/Data"
+    # data_path = f"/zhome/e1/d/168534/Desktop/Bachelor_PINN/PINN_Bachelor/Data"
+    data_path = f"/home/madshh7/PINN_Bachelor/Data"
     key = "pt1"
-    import torch
-    angle = torch.tensor(float(pi) - float(pi/8))
-    rot_matrix = torch.tensor([
-            [torch.cos(angle), -torch.sin(angle), 0],
-            [torch.sin(angle), torch.cos(angle), 0],
-            [0, 0, 1]
-            ])
-    
+
+    angle = (pi / 2) + theta
+    rot_matrix = (
+        [float(cos(angle)), float(-sin(angle)), 0],
+        [float(sin(angle)), float(cos(angle)), 0],
+        [0, 0, 1]
+    )
+
+    translate= ([
+        0,
+        inlet_pipe_length_range[-1],
+        0
+    ])
+
     input, output, nr_points = get_data(
         df_path= os.path.join(data_path, f"U0{key}_Laminar.csv"),
         desired_input_keys=["x", "y", "z"],
@@ -143,7 +149,7 @@ def run(cfg: ModulusConfig) -> None:
         desired_output_keys=["u", "v", "w", "p"],
         original_output_keys=["Velocity[i] (m/s)", "Velocity[j] (m/s)", "Velocity[k] (m/s)"],
         rotation_matrix= rot_matrix,
-        translate=(Pipe.bend.bend_planes_center[-1][0], Pipe.bend_planes_center[-1][1], Pipe.bend_planes_center[-1][2])
+        translation=translate
     )
     
     # flow_data = np.full((nr_points, 1))