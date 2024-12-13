f1a3f63d2e9c1c6ade387bc6c5021279da8ff791
MadsHH7 <madsholbek.holm@gmail.com>
2024-12-13 21:26:30 +0100

    Split data

---
+++ Data/2D/Sample_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sympy import cos, sin, pi
+import numpy as np
+
+key = "01"
+pct_data = 5 / 100 # Split into 5% Data
+
+df = pd.read_csv(f'bend_data_mvel{key}.csv')
+
+train_df = df.sample(frac = pct_data, random_state=42)
+validation_df = df.drop(train_df.index)
+
+train_df.to_csv(f'bend_data_mvel{key}_train.csv', index=False)
+validation_df.to_csv(f'bend_data_mvel{key}_validation.csv', index=False)
